f3371a33ae4697c8b343fc53b643ebeb4e362825
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2025-02-05 17:10:43 -0800

    changing name to Euler_dot_net - step 2

--- Hello2/Main.cs
+++ Hello2/Main.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel;
-
-namespace mba.Hello
+﻿namespace mba.Hello
 {
     public class Main
     {
@@ -75,7 +73,7 @@ namespace mba.Hello
                 {
                     if (i % primes[j] == 0)
                     {
-                        if (primes[j] == candidate) 
+                        if (primes[j] == candidate)
                             prime_in_list = true;
                         is_prime = false;
                         break;
@@ -94,20 +92,40 @@ namespace mba.Hello
         public bool IsPalindrome(long candidate)
         {
             string strCand = candidate.ToString();
-            string first;
-            string last;
-            int strCandLen;
-            while (strCand.Length > 0)
+            int first = 0;
+            int last = strCand.Length - 1;
+            while (first < last)
             {
-                if (strCand.Length == 1)
-                    break; // a single char is it's own mirror
-                first = strCand[0].ToString();
-                strCandLen = strCand.Length;
-                last = strCand[strCandLen - 1].ToString();
-                if (first != last)
-                    return false;
-                strCand = strCand.Substring(1, strCandLen - 2);
+                //if (strCand[first] != strCand[last]) return false;
+                //else
+                //{
+                //    first++;
+                //    last--;
+                //}
+                if (strCand[first] == strCand[last])
+                {
+                    first++;
+                    last--;
+                }
+                else return false;
             }
+            return true;
+
+            //string strCand = candidate.ToString();
+            //string first;
+            //string last;
+            //int strCandLen;
+            //while (strCand.Length > 0)
+            //{
+            //    if (strCand.Length == 1)
+            //        break; // a single char is it's own mirror
+            //    first = strCand[0].ToString();
+            //    strCandLen = strCand.Length;
+            //    last = strCand[strCandLen - 1].ToString();
+            //    if (first != last)
+            //        return false;
+            //    strCand = strCand.Substring(1, strCandLen - 2);
+            //}
 
             return true;
         }
@@ -144,7 +162,7 @@ namespace mba.Hello
                 for (long i = 2; i <= max; i++)
                 {
                     bool local_true = true;
-                    if(candidate % i != 0)
+                    if (candidate % i != 0)
                     {
                         candidate++;
                         local_true = false;
@@ -158,5 +176,269 @@ namespace mba.Hello
             return candidate;
         }
 
+        public int E06_a_SumOfTheSquares(int max)
+        {
+            int total = 0;
+            for (int i = 0; i <= max; i++)
+            {
+                total += i * i;
+            }
+            return total;
+        }
+
+        public int E06_b_SquareOfTheSums(int max)
+        {
+            int total = 0;
+            for (int i = 0; i <= max; i++)
+            {
+                total += i;
+            }
+            total *= total;
+            return total;
+        }
+
+        public int E06_c_SumSuareDifference(int max)
+        {
+            int square = E06_b_SquareOfTheSums(max);
+            int sum = E06_a_SumOfTheSquares(max);
+            int result = square - sum;
+            return result;
+        }
+
+        public int E07_a_GetNthPrime_Works(int max)
+        {
+            List<int> primes = new List<int>();
+            primes.Add(2);
+            int candidate = 2;
+            while (primes.Count < max)
+            {
+                candidate++;
+                bool isPrime = true;
+                foreach (int prime in primes)
+                {
+                    if (candidate % prime == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) primes.Add(candidate);
+            }
+            int retval = primes[max - 1];
+            return retval;
+        }
+
+        public long E08_a_GetLargestProductInSeries(string sequenceOfNumbers, int numOChars)
+        {
+            long winner = 0;
+            int lenOString = sequenceOfNumbers.Length;
+            for (int i = 0; i <= lenOString - numOChars; i++)
+            {
+                long localTotal = 1;
+                int start = i;
+                int end = i + numOChars;
+                for (int j = start; j < end; j++)
+                {
+                    localTotal *= int.Parse(sequenceOfNumbers.Substring(j, 1));
+                }
+                if (localTotal > winner) winner = localTotal;
+            }
+            return winner;
+        }
+
+
+
+        public int[] E09_a_FindSpecialPythagoreanTriplet(int target)
+        {
+            int[] retval = new int[4];
+            double ftarget = (double)target;
+            int a = 0, b = 0, c_int = 0;
+            double c_squared = (double)0.0;
+            double c = (double)0.0;
+            bool isTarget = false;
+            for (a = 3; a < target; a++)
+            {
+                for (b = a + 1; b < target; b++)
+                {
+                    c_squared = a * a + b * b;
+                    c = Math.Sqrt(c_squared);
+                    if (a + b + Math.Ceiling(c) > ftarget) break;
+                    bool cIsAWholeNumber = (c % 1) < double.Epsilon;
+                    if (cIsAWholeNumber)
+                    {
+                        c_int = (int)c;
+                        int sumOfSides = a + b + c_int;
+                        isTarget = (sumOfSides == target);
+                        retval = new int[4] { (a * b * c_int), a, b, c_int };
+                        //Console.WriteLine($"A = {retval[1]}, B = {retval[2]}, C = {retval[3]}, total = {retval[0]}");
+                        if (isTarget) break;
+                    }
+                }
+                if (isTarget) break;
+            }
+            return retval;
+        }
+
+        public bool SEL_01_findintarget(string source, string target)
+        {
+            bool foundit = false;
+            int endBuffer = source.Length - target.Length;
+            for (int i = 0; i < endBuffer; i++)
+            {
+                bool workssofar = true;
+                for (int j = i; j < i + target.Length - 1; j++)
+                {
+                    string s = source[i + j].ToString();
+                    string t = target[j].ToString();
+                    if (s != t)
+                    {
+                        workssofar = false;
+                        break;
+                    }
+                }
+                if (workssofar)
+                {
+                    foundit = true;
+                    break;
+                }
+            }
+            return foundit;
+        }
+
+        public List<int> E10_a_GetAllPrimesBelow(int max)
+        {
+            List<int> primes = new List<int>();
+            primes.Add(2);
+            primes.Add(3);
+            int current = 3;
+            while (current < max)
+            {
+                bool isPrime = true;
+                current += 2;
+                //Console.WriteLine($"processing current = {current} of {max} = {current / max * 100}");
+                for (int i = 0; i < primes.Count; i++)
+                {
+                    if (current % primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) primes.Add(current);
+            }
+            return primes;
+        }
+
+        public long E10_b_GetSumOfIntList(List<int> primes)
+        {
+            long sum = 0;
+            foreach (int i in primes) sum += (long)i;
+            return sum;
+        }
+
+        public int[,] E11_a_ConvertStringToIntGrid(string myString, int rows, int columns)
+        {
+            string[] rawString = myString.Split(' ');
+            int[] rawInts = Array.ConvertAll(rawString, s => int.Parse(s));
+            int[,] retval = new int[rows, columns];
+            int cursor = 0;
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    retval[i, j] = rawInts[cursor++];
+            return retval;
+        }
+
+        public int E11_b_GetLargestHorizontalProductFromGrid(int[,] grid)
+        {
+            int rows = grid.GetLength(0);
+            int columns = grid.GetLength(1);
+            int winner = 0;
+            for (int i = 0; i < rows; ++i)
+                for (int j = 0; j < columns - 1; j++)
+                {
+                    int candidate = grid[i, j] * grid[i, j + 1];
+                    if (candidate > winner)
+                        winner = candidate;
+                }
+            return winner;
+        }
+
+        public int E11_c_GetLargestVerticleProductFromGrid(int[,] grid)
+        {
+            int rows = grid.GetLength(0);
+            int columns = grid.GetLength(1);
+            int winner = 0;
+            for (int i = 0; i < rows - 1; ++i)
+                for (int j = 0; j < columns; j++)
+                {
+                    int candidate = grid[i, j] * grid[i + 1, j];
+                    if (candidate > winner)
+                        winner = candidate;
+                }
+            return winner;
+        }
+
+        public int E11_d_GetLargestDiagonalProductFromGrid(int[,] grid)
+        {
+            int rows = grid.GetLength(0);
+            int columns = grid.GetLength(1);
+            int winner = 0;
+            for (int i = 0; i < rows - 1; ++i)
+                for (int j = 0; j < columns; j++)
+                {
+                    int diagBack = 0;
+                    int diagForward = 0;
+                    if (j > 0)
+                        diagBack = grid[i, j] * grid[i + 1, j - 1];
+                    if (j < columns - 1)
+                        diagForward = grid[i, j] * grid[i + 1, j + 1];
+                    if (diagBack > winner)
+                        winner = diagBack;
+                    if (diagForward > winner)
+                        winner = diagForward;
+                }
+            return winner;
+        }
+
+
+
+        public int E11_e_GetLargestProductFromGrid_Len2(int[,] grid, int length)
+        {
+            int rows = grid.GetLength(0);
+            int columns = grid.GetLength(1);
+            int winner = 0;
+            int buffer = length - 1;
+            for (int i = 0; i < rows; ++i)
+                for (int j = 0; j < columns; j++)
+                {
+                    int diagBack = 1;
+                    int diagForward = 1;
+                    int horizontal = 1;
+                    int vertical = 1;
+                    bool isHighEnough = (i < rows - buffer);
+                    bool isRightEnough = (j >= buffer);
+                    bool isLeftEnough = (j < columns - buffer);
+                    for (int offset = 0; offset < length; offset++)
+                    {
+                        if (isHighEnough)
+                            vertical *= grid[i + offset, j];
+                        if (isLeftEnough)
+                            horizontal *= grid[i, j + offset];
+                        if (isRightEnough && isHighEnough)
+                            diagBack *= grid[i + offset, j - offset];
+                        if (isLeftEnough && isHighEnough)
+                            diagForward *= grid[i + offset, j + offset];
+                    }
+                    List<int> candidates = new List<int>();
+                    candidates.Add(vertical);
+                    candidates.Add(horizontal);
+                    candidates.Add(diagBack);
+                    candidates.Add(diagForward);
+                    int candidateMax = candidates.Max();
+                    if (candidateMax > winner)
+                        winner = candidateMax;
+                }
+            return winner;
+        }
     }
 }
--- Hello2_Tests/Main_Tests.cs
+++ Hello2_Tests/Main_Tests.cs
@@ -1,14 +1,13 @@
 namespace mba.Hello
 {
-    
-    public class Tests
+
+    public class UnitTests
     {
         Main o;
         [SetUp]
         public void Setup()
         {
             o = new Main();
-
         }
 
         [Test]
@@ -111,14 +110,6 @@ namespace mba.Hello
         }
 
         [Test]
-        public void E04_d_Largest_Palindrome_Product()
-        {
-            long actual = o.E04_GetLargestPalindromeByDigitCount(4);
-            Assert.That(actual, Is.EqualTo(99000099), "failure of 4 digit control ");
-
-        }
-
-        [Test]
         public void E05_a_Smallest_Multiple()
         {
             long actual = o.E05_GetSmallestMultiple(4);
@@ -133,11 +124,241 @@ namespace mba.Hello
         }
 
         [Test]
-        public void E05_c_Smallest_Multiple()
+        public void E06_a_SumOfTheSquares_Works()
         {
-            long actual = o.E05_GetSmallestMultiple(20);
-            Assert.That(actual, Is.EqualTo(232792560), "20 should yield 232792560");
+            int actual = o.E06_a_SumOfTheSquares(10);
+            Assert.That(actual, Is.EqualTo(385), "10 should yield 385");
         }
+
+        [Test]
+        public void E06_b_SquareOfTheSums_Works()
+        {
+            int actual = o.E06_b_SquareOfTheSums(10);
+            Assert.That(actual, Is.EqualTo(3025), "10 should yield 3025");
+        }
+
+        [Test]
+        public void E06_c_SumSuareDifference_Works()
+        {
+            int actual = o.E06_c_SumSuareDifference(10);
+            Assert.That(actual, Is.EqualTo(2640), "10 should yield 2640");
+        }
+
+
+        [Test]
+        public void E06_c_SumSuareDifference_Final()
+        {
+            int actual = o.E06_c_SumSuareDifference(100);
+            Assert.That(actual, Is.EqualTo(25164150), "10 should yield 25164150");
+        }
+
+        [Test]
+        public void E07_a_GetNthPrime_Works()
+        {
+            int actual = o.E07_a_GetNthPrime_Works(6);
+            Assert.That(actual, Is.EqualTo(13), "The 6th prime should be 13");
+        }
+
+        [Test]
+        public void E07_b_GetNthPrime_Works()
+        {
+            int actual = o.E07_a_GetNthPrime_Works(10001);
+            Assert.That(actual, Is.EqualTo(104743), "The 10,001st prime should be 104743");
+        }
+
+        [Test]
+        public void E08_a_GetLargestProductInSeries()
+        {
+            string candidate = "123456789123";
+            long actual = o.E08_a_GetLargestProductInSeries(candidate, 4);
+            Assert.That(actual, Is.EqualTo(3024), "Largest product should be 3024");
+        }
+
+        [Test]
+        public void E08_b_GetLargestProductInSeries()
+        {
+            string candidate = "123456789";
+            long actual = o.E08_a_GetLargestProductInSeries(candidate, 4);
+            Assert.That(actual, Is.EqualTo(3024), "Largest product should be 3024");
+        }
+
+        [Test]
+        public void E08_c_GetLargestProductInSeries()
+        {
+            string candidate = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+            long actual = o.E08_a_GetLargestProductInSeries(candidate, 4);
+            Assert.That(actual, Is.EqualTo(5832), "Largest product should be 5832");
+        }
+
+        [Test]
+        public void E08_d_GetLargestProductInSeries()
+        {
+            string candidate = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+            Assert.That(candidate.Length, Is.EqualTo(1000), "candidate length should be 1000");
+            long actual = o.E08_a_GetLargestProductInSeries(candidate, 13);
+            Assert.That(actual, Is.EqualTo(23514624000), "Largest product should be 23514624000");
+        }
+
+        [Test]
+        public void E09_a_FindSpecialPythagoreanTriplet_Works()
+        {
+            int expected = 60; // 3 + 4 + 5 = 12 - 3 * 4 * 5 = 60
+            int[] actual = o.E09_a_FindSpecialPythagoreanTriplet(12);
+            Console.WriteLine($"A = {actual[1]}, B = {actual[2]}, C = {actual[3]}");
+            Assert.That(actual[0], Is.EqualTo(expected), "sum of 12 = product of 60");
+        }
+
+        [Test]
+        public void E09_b_FindSpecialPythagoreanTriplet_Works()
+        {
+            int expected = 31875000; // 3 * 4 * 5 = 60
+            int[] actual = o.E09_a_FindSpecialPythagoreanTriplet(1000);
+            Console.WriteLine($"A = {actual[1]}, B = {actual[2]}, C = {actual[3]}");
+            Assert.That(actual[0], Is.EqualTo(expected), "to do something");
+        }
+
+        [Test]
+        public void E10_a_GetAllPrimesBelow_Works()
+        {
+            int expected = 4; // number of primes below 8
+            List<int> actual = o.E10_a_GetAllPrimesBelow(8);
+            Assert.That(actual.Count, Is.EqualTo(expected), "just getting the first 4 primes");
+        }
+
+        [Test]
+        public void E10_c_GetSumOfIntList_Works()
+        {
+            long expected = 17; // sum of the primes below 8
+            List<int> primes = o.E10_a_GetAllPrimesBelow(8);
+            long actual = o.E10_b_GetSumOfIntList(primes);
+            Assert.That(actual, Is.EqualTo(expected), "just getting the first 4 primes");
+        }
+
+        [Test]
+        public void E11_a_ReadStringAsGrid_Works()
+        {
+            int[,] expected = { { 08, 02, 97 }, { 22, 3, 4 } };
+            int rows = 2;
+            int columns = 3;
+            //  8  2 97
+            // 22  3  4
+            string myString = "08 02 97 22 03 04";
+            int[,] actual = o.E11_a_ConvertStringToIntGrid(myString, rows, columns);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void E11_b_FindLargestProduct_Horizontal_Works()
+        {
+            int rows = 2;
+            int columns = 3;
+            //  8  2 97
+            // 22  3  4
+            string myString = "08 02 97 22 03 04";
+            int[,] grid = o.E11_a_ConvertStringToIntGrid(myString, rows, columns);
+            Assert.That(2, Is.EqualTo(grid.GetLength(0)), "get rows");
+            Assert.That(3, Is.EqualTo(grid.GetLength(1)), "get columns");
+            int actual = o.E11_b_GetLargestHorizontalProductFromGrid(grid);
+            int expected = 194;
+            //expected = 7;
+            Assert.That(actual, Is.EqualTo(expected), "should find 2*97");
+        }
+
+        [Test]
+        public void E11_c_FindLargestProduct_Verticle_Works()
+        {
+            int rows = 2;
+            int columns = 3;
+            //  8  2 97
+            // 22  3  4
+            string myString = "08 02 97 22 03 04";
+            int[,] grid = o.E11_a_ConvertStringToIntGrid(myString, rows, columns);
+            int actual = o.E11_c_GetLargestVerticleProductFromGrid(grid);
+            int expected = 388;
+            Assert.That(actual, Is.EqualTo(expected), "should find 4*97");
+        }
+
+        [Test]
+        public void E11_d_FindLargestProduct_Diagonal_Works()
+        {
+            int rows = 2;
+            int columns = 3;
+            //  8  2 97
+            // 22  3  4
+            string myString = "08 02 97 22 03 04";
+            int[,] grid = o.E11_a_ConvertStringToIntGrid(myString, rows, columns);
+            int actual = o.E11_d_GetLargestDiagonalProductFromGrid(grid);
+            int expected = 291;
+            Assert.That(actual, Is.EqualTo(expected), "should find 3*97");
+        }
+
+        [Test]
+        public void E11_e_FindLargestProduct_2_Works()
+        {
+            int rows = 2;
+            int columns = 3;
+            //  8  2 97
+            // 22  3  4
+            string myString = "08 02 97 22 03 04";
+            int[,] grid = o.E11_a_ConvertStringToIntGrid(myString, rows, columns);
+            int actual = o.E11_e_GetLargestProductFromGrid_Len2(grid, 2);
+            int expected = 388;
+            Assert.That(actual, Is.EqualTo(expected), "should find 4*97");
+        }
+
+        [Test]
+        public void E11_e_FindLargestProduct_3_Works()
+        {
+            int rows = 2;
+            int columns = 3;
+            //  8  2 97
+            // 22  3  4
+            string myString = "08 02 97 22 03 04";
+            int[,] grid = o.E11_a_ConvertStringToIntGrid(myString, rows, columns);
+            int actual = o.E11_e_GetLargestProductFromGrid_Len2(grid, 3);
+            int expected = 1552;
+            Assert.That(actual, Is.EqualTo(expected), "should find 8*2*97");
+        }
+
+        [Test]
+        public void E11_f_FindLargestProduct_All_Works()
+        {
+            int rows = 4;
+            int columns = 4;
+            //  8  2 97 14
+            // 22  3  4 12
+            // 07 15 23 19
+            // 72 05 11 21
+            string myString = "08 02 97 14 22 03 04 12 07 15 23 19 72 05 11 21";
+            int[,] grid = o.E11_a_ConvertStringToIntGrid(myString, rows, columns);
+            int actual = o.E11_e_GetLargestProductFromGrid_Len2(grid, 2);
+            int expected = 1358;
+            Assert.That(actual, Is.EqualTo(expected), "should find 14*97");
+        }
+
+
+        [Test]
+        public void E11_g_FindLargestProduct_20x20_Works()
+        {
+            string myString = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
+            int rows = 20;
+            int columns = 20;
+            int[,] grid = o.E11_a_ConvertStringToIntGrid(myString, rows, columns);
+            int actual = o.E11_e_GetLargestProductFromGrid_Len2(grid, 4);
+            int expected = 70600674;
+            Assert.That(actual, Is.EqualTo(expected), "should find 70600674");
+
+        }
+
+
+        [Test]
+        public void SEL_01_findintarget_Works()
+        {
+            bool expected = true;
+            bool actual = o.SEL_01_findintarget("adabc", "ab");
+            Assert.That(actual, Is.EqualTo(expected), "expected true");
+        }
+
 
 
 
@@ -149,20 +370,61 @@ namespace mba.Hello
             primes.Add(2);
             primes.Add(3);
             primes.Add(5);
-            primes.Add(7);  
+            primes.Add(7);
             primes.Add(11);
-            Console.WriteLine($"primes length = {primes.Count} and the last prime is {primes[primes.Count-1]}");
+            Console.WriteLine($"primes length = {primes.Count} and the last prime is {primes[primes.Count - 1]}");
             int[] other_primes = { 2, 3, 5, 7, 11 };
             primes2 = new List<int>(other_primes);
             Assert.AreEqual(primes, primes2);
             primes2 = o.GetPrimesLessThanCandidate(primes, 3);
             primes2 = o.GetPrimesLessThanCandidate(primes, 32);
             int expected = 31;
-            int actual = primes2[primes2.Count-1];
+            int actual = primes2[primes2.Count - 1];
             Console.WriteLine($"primes2 length = {primes2.Count} and the last prime is {primes2[primes2.Count - 1]}");
             Assert.That(actual, Is.EqualTo(expected));
+        }
 
 
+    }
+
+    public class IntegrationTests
+    {
+        Main o;
+        [SetUp]
+        public void Setup()
+        {
+            o = new Main();
+        }
+
+        [Test]
+        public void E04_d_Largest_Palindrome_Product()
+        {
+            long actual = o.E04_GetLargestPalindromeByDigitCount(4);
+            Assert.That(actual, Is.EqualTo(99000099), "failure of 4 digit control ");
+        }
+
+        [Test]
+        public void E05_c_Smallest_Multiple()
+        {
+            long actual = o.E05_GetSmallestMultiple(20);
+            Assert.That(actual, Is.EqualTo(232792560), "20 should yield 232792560");
+        }
+
+        [Test]
+        public void E10_b_GetAllPrimesBelow_Works()
+        {
+            int expected = 148933; // number of primes below 2,000,000
+            List<int> actual = o.E10_a_GetAllPrimesBelow(2000000);
+            Assert.That(actual.Count, Is.EqualTo(expected), "just getting the first 4 primes");
+        }
+
+        [Test]
+        public void E10_d_GetSumOfIntList_Works()
+        {
+            long expected = 142913828922; // sum of the primes below 2000000
+            List<int> primes = o.E10_a_GetAllPrimesBelow(2000000);
+            long actual = o.E10_b_GetSumOfIntList(primes);
+            Assert.That(actual, Is.EqualTo(expected), "just getting the first 4 primes");
         }
 
 
